61ebbfab23480f7cbfdf98c3ed23c79992a3dbdb
nilptr <nilptr.js@gmail.com>
2022-10-02 22:29:48 +0800

    chore: add minified build

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,17 +1,25 @@
 import type { RollupOptions } from 'rollup';
 
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 const config: RollupOptions = {
   input: 'src/index.ts',
-  output: {
+  output: [{
     // dir: 'dist/',
     file: 'dist/petite-vue-transition.js',
     format: 'umd',
     sourcemap: true,
     name: 'PetiteVueTransition',
     exports: 'default',
-  },
+  }, {
+    file: 'dist/petite-vue-transition.min.js',
+    format: 'umd',
+    sourcemap: true,
+    name: 'PetiteVueTransition',
+    exports: 'default',
+    plugins: [terser()],
+  }],
   plugins: [typescript()],
 };
 
